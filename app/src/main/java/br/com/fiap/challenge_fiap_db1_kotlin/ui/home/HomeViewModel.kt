c17c02433557ac801168bf9b90187ce9a83c5eac
SantosRodrigo89 <rodrigo.v.santos89@gmail.com>
2024-04-14 13:04:44 -0300

    Initial commit

---
+++ app/src/main/java/br/com/fiap/challenge_fiap_db1_kotlin/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package br.com.fiap.challenge_fiap_db1_kotlin.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
